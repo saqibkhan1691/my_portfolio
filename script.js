f32b4e68e27fff4fcf3b51f32a5ba8837aafb0e1
Saqib Khan <saqibkhan4428960@gmail.com>
2026-02-06 09:33:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const navbar = document.querySelector(".navbar");
 
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        navbar.style.borderBottom = "1px solid #1e293b";
+    } else {
+        navbar.style.borderBottom = "none";
+    }
+});
